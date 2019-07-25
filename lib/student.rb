ecb1218a6319d59877de2658061326b3910a1b46
saazim1123 <saazim1123@gmail.com>
2019-07-25 13:40:47 +0100

    initialized and self.create table completed
    Initialization
    self.create_table
    self.drop_ table
    (Completed)

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,36 @@
 class Student
 
+  attr_reader :id
+  attr_accessor :name, :grade
+
+  
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      create table if not exists students (
+        id integer primary key,
+        name text
+        grade integer
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      drop table students
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
   
 end
